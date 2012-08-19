471c82c58b0d312eea395ce3f49d61d80ce2a7f5
Chairshot215 <chairshot215@chairshot215-desktop.(none)>
2012-08-19 18:35:58 -0400

    Disabled surface dithering, can be enabled in CM settings and increased dalvik vm heap-size for better performance.

--- motorola/triumph/triumph.mk
+++ motorola/triumph/triumph.mk
@@ -196,8 +196,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Perfomance tweaks and misc
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.execution-mode=int:jit \
-    dalvik.vm.heapsize=32m \
-    persist.sys.use_dithering=1 \
+    dalvik.vm.heapsize=48m \
+    persist.sys.use_dithering=0 \
     ro.sf.lcd_density=240 \
     ro.compcache.default=0
 
